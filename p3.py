65a0a2f8c32424c2cc1fcfd11d31cb440e89be10
RunyShark <spartadragon117@live.com.mx>
2023-07-15 20:48:26 -0600

    refactor

--- p3.py
+++ p3.py
@@ -1,6 +1,4 @@
 trabajador = input('nombre del colaborador: ')
-venta = input('monto total de venta: ')
-venta = float(venta)
-venta = round(venta)
+venta = float(input('monto total de venta: '))
 
 print(f'Hola! {trabajador} tu comicion es: {round(venta * 0.13, 2)}')
