5a4a98f295463820ad47cc732d03359be0657dd7
RunyShark <spartadragon117@live.com.mx>
2023-07-15 19:56:44 -0600

    op

---
+++ FNumber.py
@@ -0,0 +1,16 @@
+x = 6
+y = 2
+z = 7
+
+print(f'{x} + {y} = {x + y}')
+print(f'{x} - {y} = {x - y}')
+print(f'{x} * {y} = {x * y}')
+print(f'{x} / {y} = {x / y}')
+
+print(f'division al piso {z} / {y} = {z // y}')
+
+print(f'Modulo {z} modulo {y} = {z % y}')
+
+print(f'Modulo {z} modulo {z} = {z % z}')
+
+print(f'La raiz cuadrada de {x}  {x ** 0.5}')
--- fStrings.py
+++ fStrings.py
@@ -1,3 +1,4 @@
 print(f'Mi edad es {24} y mi matricula es {92.3}')
 print('Mi edad es {} y mi matricula es {}'.format(30, 92.2))
 print('la suma de {} y {} es = {}'.format(10, 3, 10 + 3))
+print(f'la suma de {10} y {3} es = {10 + 3}')
