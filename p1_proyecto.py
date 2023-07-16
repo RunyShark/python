1286faadf066e116fbc6651bcfb024d3bbd8d76f
RunyShark <spartadragon117@live.com.mx>
2023-07-15 18:52:20 -0600

    f

--- p1_proyecto.py
+++ p1_proyecto.py
@@ -1,3 +1,3 @@
-print("El nombre de tu marca es: " + input('intruduce el nombre de tu mascota: ') + input('Introduce los 2 ultimos dijitos del año en el que naciste: ') )
+print("El nombre de tu marca es: " + input('introduce el nombre de tu mascota: ') + input('Introduce los 2 últimos  dígitos  del año en el que naciste: ') )
 
 
