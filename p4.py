8cb86e0d4c60f487a8f0cff1eb92175da726412e
RunyShark <spartadragon117@live.com.mx>
2023-07-17 21:00:30 -0600

    p4

--- p4.py
+++ p4.py
@@ -4,7 +4,7 @@ letter.append(input('entre 1 letter ').lower())
 letter.append(input('entre 2 letter ').lower())
 letter.append(input('entre 3 letter ').lower())
 
-print(f'letter 1 {user_text.count(letter[0])} letter 2 {user_text.count(letter[1])} letter 3 {user_text.count(letter[2])} \n  how many words {len(user_text)} firs letter {user_text[0]} ood letter {user_text[-1]} \n Phyton exist in text ? {user_text.find("Python") > 1}')
+print(f'letter 1 {user_text.count(letter[0])} letter 2 {user_text.count(letter[1])} letter 3 {user_text.count(letter[2])} \n  how many words {len(user_text.split())} firs letter {user_text[0]} ood letter {user_text[-1]} \n Phyton exist in text ? {user_text.find("Python") > 1}')
 
 
 
