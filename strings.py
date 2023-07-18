af7ace59a083b6e3fe2915098d9cfab4f49ac52a
RunyShark <spartadragon117@live.com.mx>
2023-07-17 19:26:58 -0600

    fragmets

--- strings.py
+++ strings.py
@@ -10,3 +10,26 @@ print(name[-1])
 print(name.index('i'))
 
 print(name.index('r', 2, 4))
+
+print(name.rindex('o'))
+
+
+base = 'red blue red'
+fragment = base[3:8]
+print(fragment)
+
+
+
+fragment = base[3:]
+print(fragment)
+
+
+base = '123456789'
+fragment = base[::2]
+print(fragment)
+
+fragment = base[::3]
+print(fragment)
+
+fragment = base[::-3]
+print(fragment)
